e95ac48dadf7eacbf2dace274d7f718586b740d7
Ayushman Dash <dash.ayushman.99@gmail.com>
2016-09-13 01:30:33 +0200

    removed unnecessary imports

--- __init__.py
+++ __init__.py
@@ -1,8 +1,1 @@
 
-"""Makes helper libraries available in the transliterate package."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from tensorflow.models.rnn.translate import data_utils
-from tensorflow.models.rnn.translate import seq2seq_model
